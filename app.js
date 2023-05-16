34e519e58779654d5450de3f54a7fee792cea3c9
RowanEssam14 <rowan1412sharaf@gmail.com>
2023-05-16 16:34:36 +0200

    TestingCarousel,creating routes dir,app.js changes

--- app.js
+++ app.js
@@ -1,42 +1,21 @@
-//importing  necessary modules from nodejs 
 const express = require('express');
-const mysql = require('mysql');
-
-//initializing the server instance for our application.
 const app = express();
 const port = 3300;
-
 
 // Setting the view engine to EJS
 app.set('view engine', 'ejs');
 
-//importing the necessary path module from nodejs
+// Import the necessary path module from Node.js
 const path = require('path');
 
-//specifying the static files directory
+// Specify the static files directory
 app.use(express.static('public'));
 
-// Create a MySQL connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'do2lKjv0lMD86OGT',
-  database: 'library_managment_system'
-});
+// Import the routes
+const booksRouter = require('./routes/books');
 
-// Connect to the MySQL database
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to the MySQL database.');
-});
-
-// Define a route to fetch books data from the database
-app.get('/books', function(req, res) {
-  connection.query('SELECT * FROM book', function(error, results, fields) {
-    if (error) throw error;
-    res.render('books', { books: results });
-  });
-});
+// Use the books route
+app.use('/books', booksRouter);
 
 // Starting the server
 app.listen(port, function() {
