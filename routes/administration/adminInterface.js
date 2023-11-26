4b32a51db248f65be477397df91b42fc73b9f49b
RowanEssam14 <rowan1412sharaf@gmail.com>
2023-11-26 22:32:10 +0100

    edit_user : changed

--- routes/administration/adminInterface.js
+++ routes/administration/adminInterface.js
@@ -17,20 +17,25 @@ connection.connect((err) => {
  });
 
 
-router.get('/', function(req, res) {
+ router.get('/', function(req, res) {
   if (req.session.loggedin && req.session.role === 'admin') {
     connection.query('SELECT * FROM role', (error, roles, fields) => {
       if (error) throw error;
       connection.query('SELECT user.*, role.role_name, role.max_borrow FROM user INNER JOIN role ON user.role_id = role.role_id WHERE is_deleted = 0 ORDER BY role.role_name ASC', (error, users, fields) => {
         console.log(roles)
         if (error) throw error;
-        res.render('administration/adminInterface', { user: users, roles: roles });
+        if (req.xhr) { // If the request is an AJAX request
+          res.json({ user: users, roles: roles }); // Send JSON data
+        } else {
+          res.render('administration/adminInterface', { user: users, roles: roles }); // Render the view
+        }
       });
     });
   } else {
     res.redirect('http://localhost:3300/adminLogin');
   }
 });
+
 
 router.post('/addUser', function(req, res) {
   console.log('add user hit');
@@ -61,20 +66,6 @@ router.delete('/delete/:id', function(req, res) {
     }
   });
 });
-
-// router.post('/update', function(req, res) {
-//   const { libraryId, role } = req.body;
-//   console.log(libraryId,role)
-
-//   connection.query('UPDATE user SET role_id = (SELECT role_id FROM `role` WHERE `role`.role_name = ?) WHERE user.library_id = ?', [role, libraryId], (error, results, fields) => {
-//     if (error) {
-//       console.error(error);
-//       res.status(500).send({ success: false, message: 'Database error' });
-//     } else {
-//       res.send({ success: true });
-//     }
-//   });
-// });
 
 
 router.post('/changePassword/:id', function(req, res) {
